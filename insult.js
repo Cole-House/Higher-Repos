8ce9a0ca46caaf74f09b2973ed38f86ce08cb8ef
guacole <30875198+guacole@users.noreply.github.com>
2017-08-21 09:21:54 -1000

    insult.js
    insult generator

---
+++ insult.js
@@ -0,0 +1,10 @@
+function sayInsult () {
+  //created function to create evnet in html//
+var realAdjective = ["bouda" , "smelly" , "scrawny" , "thicc", "cringy"];
+//created array for 1st part of insult//
+var realNoun = ["head ass", "beta" , "troll", "Stevie Stephens", "booger"];
+// created array for 2nd part of insult//
+window.alert(realAdjective[Math.floor(Math.random()*5)] + " " + realNoun[Math.floor(Math.random()*5)]);
+   // popup presenting the random 2 part insult from both arrays//
+ 
+}
