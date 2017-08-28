6db6e0563a1cd828c76d25c3ad7306cd9b4d3ef1
guacole <30875198+guacole@users.noreply.github.com>
2017-08-28 08:50:10 -1000

    Update orderName2.js

--- orderName2.js
+++ orderName2.js
@@ -1,9 +1,11 @@
 var x = prompt("enter firstName, lastname, age, grade");
-//creates user event where user can call upon the
+//creates user event where user can call upon the objects within var students//
 var student = {
  firstName:"Cole",
  lastName:"House", 
  age:15,
  grade:"10th",
 }
+//var students that holds objects within it//
 window.alert(student[x])
+//pops up the user selected object from var students//
