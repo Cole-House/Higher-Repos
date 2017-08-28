ec75ba336b37dffbc3bbb9bce6a15eda41203b2b
guacole <30875198+guacole@users.noreply.github.com>
2017-08-28 08:47:48 -1000

    modifyObjects.js
    how to change an object with prompt

---
+++ modifyObject.js
@@ -0,0 +1,11 @@
+var x = prompt("Whats his new first name");
+//prompts user to enter var students new firstName//
+var student = {
+ firstName:"Cole",
+ lastName:"House", 
+ age:15,
+ grade:"10th",
+}
+//var students with objects (firstName, age, etc.)//
+window.alert(student.firstName = x)
+//popup of replaced firstName with var x//
