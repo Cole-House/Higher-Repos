0897014f93ea0e8181d371592d1f746ca1a4b40e
guacole <30875198+guacole@users.noreply.github.com>
2017-08-28 08:40:33 -1000

    orderName2.js
    finish comments

---
+++ orderName2.js
@@ -0,0 +1,9 @@
+var x = prompt("enter firstName, lastname, age, grade");
+//creates user event where user can call upon the
+var student = {
+ firstName:"Cole",
+ lastName:"House", 
+ age:15,
+ grade:"10th",
+}
+window.alert(student[x])
