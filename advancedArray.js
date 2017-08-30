7ce63c62705b60dddb395f128440d6afcdd9970b
guacole <30875198+guacole@users.noreply.github.com>
2017-08-30 08:51:17 -1000

    advancedObject.js
    puts an array inside an object and calls upon one

---
+++ advancedArray.js
@@ -0,0 +1,22 @@
+var students = {
+ firstName:"Cole",
+ age: 12,
+ grade: 10,
+ friends: ["Chance", "shithead", "Yogi"]
+};
+var children = {
+ firstName:"House",
+ age: 21,
+ grade: 11,
+ friends: ["Connor","Floyd", "Yachty"]
+};
+var adults = {
+ firstName:"Mister",
+ age: 25,
+ grade: 9,
+ friends: ["Ren","Jamir","Reimon"]
+};
+
+var x = [students,children,adults];
+
+window.alert(x[1].friends[2]);
