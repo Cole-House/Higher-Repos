e6fe2d9440f28569436ad31a2f6fe44122322286
guacole <30875198+guacole@users.noreply.github.com>
2017-09-19 09:04:12 -1000

    Create styleBackground.js
    basic styling in js, use the syntax!

---
+++ styleBackground.js
@@ -0,0 +1,8 @@
+<button onclick= "morphBack()">
+Morphin Time
+</button>
+
+function morphBack(){
+document.body.style.background = 'url("https://i.ytimg.com/vi/imr_PgqPgbE/maxresdefault.jpg")';
+document.body.style.backgroundSize ='cover';
+}
